7f581fc510c47e9babcff9c0fd9b77f7b510a533
Kevin ivan Chretien <68036359+kevinivanchretien@users.noreply.github.com>
2020-07-31 15:46:58 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,45 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+function mostrar() {
+	let Estacion = document.getElementById("txtIdEstacion").value;
+	let Provincia = document.getElementById("txtIdDestino").value;
+
+	if (Estacion == "Invierno") {
+		switch (Provincia) {
+			case "Bariloche":
+				alert("Se viaja");
+				break;
+			default:
+				alert("No se viaja");
+		}
+	}
+	else if (Estacion == "Verano") {
+		switch (Provincia) {
+			case "Mar del plata":
+			case "Cataratas":
+				alert("Se viaja");
+				break;
+
+			default:
+				alert("No se viaja");
+
+		}
+
+	}
+	else if (Estacion == "Primavera") {
+		switch (Provincia) {
+			case "Bariloche":
+				alert("No se viaja");
+				break;
+			default:
+				alert("Se viaja");
+
+		}
+	}
+	else if (Estacion == "Otoño") {
+		alert("se viaja");
+	}
+
+
+	//	var estacionIngresada =txtIdEstacion.value;
+	//	alert(estacionIngresada);
 
 }//FIN DE LA FUNCIÓN
